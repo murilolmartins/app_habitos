b94feeeb31cb5a0077a48d1486ee25cc1b8be385
MicaelD <micaeldiniz6@gmail.com>
2021-12-09 09:59:48 -0300

    Test: removing LoginForm from App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import LoginForm from './components/LoginForm';
+
 function App() {
   return (
     <div className="App">
       <div className="App-Container">
-        <LoginForm/>
+        
       </div>
     </div>
   );
