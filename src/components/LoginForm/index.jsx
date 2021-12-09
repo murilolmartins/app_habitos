61bb572fc1d32a50f73aa0a3b82fb3b59ab52542
MicaelD <micaeldiniz6@gmail.com>
2021-12-09 11:00:34 -0300

    Feature: useForm and schema for Login

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -1,17 +1,32 @@
-import Button from './../Button';
-import Input from './../Input';
-import {Container,Form,Title} from './styles';
-const LoginForm = ()=>{
+import Button from "./../Button";
+import Input from "./../Input";
+import { Container, Form, Title } from "./styles";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const LoginForm = () => {
+  const schema = yup.object().shape({
+    email: yup.string().email("Email inválido").required("Email obrigatório"),
+    password: yup.string().required("Senha obrigatória"),
+  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+  const handleForm = (data) => {};
   return (
     <Container>
       <Title>Entrar com uma conta existente</Title>
-      <Form>
-        <Input type="text" placeholder='Email' />
-        <Input type="text" placeholder='Senha'/>
-        <Button>Entrar</Button>
+      <Form onsubmit={handleSubmit(handleForm)}>
+        <Input type="text" placeholder="Email" {...register("email")} />
+        <Input type="text" placeholder="Senha" {...register("password")} />
+        <Button type="submit">Entrar</Button>
       </Form>
     </Container>
   );
-
 };
 export default LoginForm;
