9a27880c436eb088d87d84d1f889bbd75b1e55af
MateusVSousa <min705555@gmail.com>
2021-12-09 14:45:02 -0300

    fix:header logo/media query

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import "./App.css";
-import Header from "./components/DesktopHeader";
-
 function App() {
   return (
     <div className="App">
-      <Header></Header>
       <div className="App-Container"></div>
     </div>
   );
--- src/components/DesktopHeader/index.jsx
+++ src/components/DesktopHeader/index.jsx
@@ -1,15 +1,17 @@
 import { Link } from "react-router-dom";
 import { Container } from "./style";
-
+import logo from "../../assets/images/logo-ukifazer.png";
 const Header = () => {
   return (
     <Container>
-      <img />
+      <figure>
+        <img src={logo} alt="Logo" />
+      </figure>
       <div>
-        <Link>Home</Link>
-        <Link>About</Link>
-        <Link>Login</Link>
-        <Link>Register</Link>
+        <Link to="/">Home</Link>
+        <Link to="/">About</Link>
+        <Link to="/">Login</Link>
+        <Link to="/">Register</Link>
       </div>
     </Container>
   );
