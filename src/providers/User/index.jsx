419857c180a8b2c6c6be77b82ca668fba06e7d74
Murilo Martins <murilolmartins1@gmail.com>
2021-12-10 14:48:30 -0300

    feat: user_context

--- src/providers/User/index.jsx
+++ src/providers/User/index.jsx
@@ -1,7 +1,31 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { AuthContext } from "../Auth";
+import jwt_decode from "jwt-decode";
+import api from "../../services/api";
 
 export const UserContext = createContext();
 
-const UserProvider = ({ children }) => {};
+const UserProvider = ({ children }) => {
+  const { token } = useContext(AuthContext);
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const id = jwt_decode(token).user_id;
+    loadUser(id);
+  }, [token]);
+
+  const loadUser = (id) => {
+    api
+      .get(`users/${id}/`)
+      .then((response) => {
+        setUser(response.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <UserContext.Provider value={{ user }}>{children}</UserContext.Provider>
+  );
+};
 
 export default UserProvider;
