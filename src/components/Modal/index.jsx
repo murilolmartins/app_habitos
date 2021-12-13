a682da272d3c6fe83ea30e3f9cf810f29f989cd8
Murilo Martins <murilolmartins1@gmail.com>
2021-12-13 19:50:03 -0300

    fix: display bug

---
+++ src/components/Modal/index.jsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+import ReactModal from "react-modal";
+
+const Modal = ({ children, isOpen, setIsOpen }) => {
+  const [modalStatus, setModalStatus] = useState(false);
+  useEffect(() => {
+    setModalStatus(isOpen);
+  }, [isOpen]);
+  return (
+    <ReactModal
+      isOpen={!!modalStatus}
+      contentLabel="onRequestClose Example"
+      onRequestClose={setModalStatus}
+      shouldCloseOnOverlayClick={true}
+      ariaHideApp={false}
+      style={{
+        content: {
+          top: "50%",
+          left: "50%",
+          right: "auto",
+          bottom: "auto",
+          marginRight: "-50%",
+          transform: "translate(-50%, -50%)",
+          background: "#fff",
+          color: "#333",
+          borderRadius: "8px",
+          width: "348px",
+          padding: "10px",
+          height: "170px",
+        },
+        overlay: {
+          background: "rgba(51, -51, -51, 0.5)",
+        },
+      }}
+    >
+      {children}
+    </ReactModal>
+  );
+};
+
+export default Modal;
